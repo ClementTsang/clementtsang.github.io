9b17a725166425964d25f8e7c7e1a6505e7a6dc8
ClementTsang <clementjhtsang@gmail.com>
2019-09-22 02:38:49 -0400

    More updates, fixed formatting of some stuff.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="app">
-    <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+    <nav
+      class="navbar is-fixed-top"
+      role="navigation"
+      aria-label="main navigation"
+    >
       <div class="navbar-brand">
-        <router-link @click.native='mobileCloseHamburger' class="navbar-item" to="/#home">
+        <router-link
+          @click.native="mobileCloseHamburger"
+          class="navbar-item"
+          to="/#home"
+        >
           <p>Clement Tsang</p>
         </router-link>
 
@@ -12,170 +20,220 @@
           class="navbar-burger burger"
           aria-label="menu"
           aria-expanded="false"
-          data-target="mainNavBar"
-          @click='toggleHamburger'
+          data-target="main-nav-bar"
+          @click="toggleHamburger"
         >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
         </a>
       </div>
 
-      <div id="mainNavBar" class="navbar-menu is-shadowless is-marginless is-paddingless">
+      <div
+        id="main-nav-bar"
+        class="navbar-menu is-shadowless is-marginless is-paddingless"
+      >
         <div class="navbar-end">
-          <router-link id="about-link" @click.native='mobileCloseHamburger' class="navbar-item" to="/#about">About</router-link>
-          <router-link id="experience-link" @click.native='mobileCloseHamburger' class="navbar-item" to="/#experience">Experience</router-link>
-          <router-link id="projects-link" @click.native='mobileCloseHamburger' class="navbar-item" to="/#projects">Projects</router-link>
+          <router-link
+            id="about-link"
+            @click.native="mobileCloseHamburger"
+            class="navbar-item"
+            to="/#about"
+          >
+            About
+          </router-link>
+          <router-link
+            id="experience-link"
+            @click.native="mobileCloseHamburger"
+            class="navbar-item"
+            to="/#experience"
+          >
+            Experience
+          </router-link>
+          <router-link
+            id="projects-link"
+            @click.native="mobileCloseHamburger"
+            class="navbar-item"
+            to="/#projects"
+          >
+            Projects
+          </router-link>
+          <router-link
+            id="contact-link"
+            @click.native="mobileCloseHamburger"
+            class="navbar-item"
+            to="/#contact"
+          >
+            Contact
+          </router-link>
         </div>
       </div>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {};
-	},
-	mounted: () => {
-		const handleScroll = navItem => {
-			const correspondingLink = document.querySelector(`#${navItem.id}-link`)
-		
-			// TODO: Add scroll spy
-		}
+  data() {
+    return {};
+  },
+  mounted: () => {
+    const handleScroll = navItem => {
+      const correspondingLink = document.querySelector(`#${navItem.id}-link`);
 
-		Array.from(document.querySelectorAll(".page-item")).forEach(handleScroll)
-	},
-	methods: {
-		toggleHamburger() {
-			const burgerEle = document.getElementById("menu-hamburger");
-			const target = burgerEle.dataset.target;
-			burgerEle.classList.toggle("is-active");
-			document.getElementById(target).classList.toggle("is-active");
-		},
-		mobileCloseHamburger() {
-			const burgerEle = document.getElementById("menu-hamburger");
-			if (burgerEle.classList.contains("is-active")) {
-				this.toggleHamburger();
-			}
-		}
-	},
+      // TODO: Add scroll spy
+    };
+
+    Array.from(document.querySelectorAll(".page-item")).forEach(handleScroll);
+  },
+  methods: {
+    toggleHamburger() {
+      const burgerEle = document.getElementById("menu-hamburger");
+      const target = burgerEle.dataset.target;
+      burgerEle.classList.toggle("is-active");
+      document.getElementById(target).classList.toggle("is-active");
+    },
+    mobileCloseHamburger() {
+      const burgerEle = document.getElementById("menu-hamburger");
+      if (burgerEle.classList.contains("is-active")) {
+        this.toggleHamburger();
+      }
+    }
+  }
 };
 </script>
 
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Quicksand|Raleway&display=swap');
+@import url("https://fonts.googleapis.com/css?family=Quicksand|Raleway&display=swap");
 
 :root {
-	--main-background:#ffffff;
-	--dark-text-colour: #1c1b20;
-	--accent-1: #ff5851;
-	--accent-2: #407B97;
-	--accent-3: #383542;
-	--accent-4: #A1E2D2;
-	--main-background-two: #f8f8f8;
+  --main-background: #ffffff;
+  --dark-text-colour: #1c1b20;
+  --accent-1: #ff5851;
+  --accent-2: #df0d0d;
+  --accent-3: #383542;
+  --accent-4: #a1e2d2;
+  --main-background-two: #f8f8f8;
 }
 
-.navbar, .navbar-menu, .navbar-item, .navbar-link, .navbar-dropdown, #menu-hamburger {
-	background-color: var(--main-background) !important;
-	color: var(--dark-text-colour) !important;
-	font-family: "Raleway", sans-serif !important;
-	font-size: 0.97em !important;
-	outline: 0;
-	border: none;
-	/*opacity: 0.95;*/  /* TODO: Add this after scroll spy is added! */
+.navbar,
+.navbar-menu,
+.navbar-item,
+.navbar-link,
+.navbar-dropdown,
+#menu-hamburger {
+  background-color: var(--main-background) !important;
+  color: var(--dark-text-colour) !important;
+  font-family: "Raleway", sans-serif !important;
+  font-size: 0.97em !important;
+  outline: 0;
+  border: none;
+  /*opacity: 0.95;*/ /* TODO: Add this after scroll spy is added! */
 }
 
 .navbar-item:hover {
-	background-color: var(--accent-1) !important;
-	color: var(--dark-text-colour) !important;
+  background-color: var(--accent-1) !important;
+  color: var(--dark-text-colour) !important;
 }
 
-.navbar-item:active, .navbar-item:focus, .on-navbar-item {
-	outline: 0;
-	border: none;
-	-moz-outline-style: none;
-	outline-style: none;
+.navbar-item:active,
+.navbar-item:focus,
+.on-navbar-item {
+  outline: 0;
+  border: none;
+  -moz-outline-style: none;
+  outline-style: none;
 }
 
 .navbar-brand > .navbar-item {
-	font-size: 1.1em !important;
+  font-size: 1.1em !important;
 }
 
 @media screen and (max-width: 1023px) {
-	.navbar-item:hover {
-		background-color: var(--accent-1) !important;
-		color: var(--main-background) !important;
-	}
+  .navbar-item:hover {
+    background-color: var(--accent-1) !important;
+    color: var(--main-background) !important;
+  }
 
-	div>.navbar-item:hover {
-		background-color: var(--accent-1) !important;
-	}
+  div > .navbar-item:hover {
+    background-color: var(--accent-1) !important;
+  }
 
-	.navbar, .navbar-menu, .navbar-item, .navbar-link, .navbar-dropdown, #menu-hamburger {
-		font-size: 0.975em !important;
-	}
+  .navbar,
+  .navbar-menu,
+  .navbar-item,
+  .navbar-link,
+  .navbar-dropdown,
+  #menu-hamburger {
+    font-size: 0.975em !important;
+  }
 
-	.navbar-brand > .navbar-item {
-		font-size: 1.1em !important;
-	}
-
+  .navbar-brand > .navbar-item {
+    font-size: 1.1em !important;
+  }
 }
 
 hr.is-divider {
-	background-color: var(--accent-1);
-	display: block;
-	height: 2px;
-	border: 0;
-	margin: 2em auto;
-	margin-top: 1em;
-	padding: 0;
+  background-color: var(--accent-1);
+  display: block;
+  height: 2px;
+  border: 0;
+  margin: 2em auto;
+  margin-top: 1em;
+  padding: 0;
 }
 
 hr.is-thin-divider {
-	background-color: var(--accent-1);
-	display: block;
-	height: 1px;
-	border: 0;
-	margin: 1.5em auto;
-	margin-top: 0.5em;
-	padding: 0;
-	width: 95%;
+  background-color: var(--accent-1);
+  display: block;
+  height: 1px;
+  border: 0;
+  margin: 1.5em auto;
+  margin-top: 0.5em;
+  padding: 0;
+  width: 95%;
 }
 
-
 .title {
-	font-size: 2.75em !important;
-	color: var(--dark-text-colour) !important;
-	font-family: "Quicksand", sans-serif !important;
-	font-weight: 300;
+  font-size: 2.75em !important;
+  color: var(--dark-text-colour) !important;
+  font-family: "Quicksand", sans-serif !important;
+  font-weight: 300;
 }
 
 .subtitle {
-	font-size: 2em !important;
-	color: var(--dark-text-colour) !important;
-	font-family: "Raleway", sans-serif !important;
+  font-size: 2em !important;
+  color: var(--dark-text-colour) !important;
+  font-family: "Raleway", sans-serif !important;
 }
 
 h3 {
-	font-size: 1.05em !important;
-	color: var(--dark-text-colour) !important;
-	font-family: "Raleway", sans-serif !important;
-	line-height: 160%;
+  font-size: 1.05em !important;
+  color: var(--dark-text-colour) !important;
+  font-family: "Raleway", sans-serif !important;
+  line-height: 160%;
 }
 
 a {
-	color: var(--accent-1);
+  color: var(--accent-1);
 }
 
 a:visited {
-	color: #8B008B;
+  color: #8b008b;
 }
 
 a:hover {
-	color: var(--accent-3);
+  color: var(--accent-3);
 }
 
+.is-divider {
+  width: 3em;
+}
+
+article {
+  width: 70%;
+  margin: 0 auto;
+}
 </style>
--- src/components/ProjectInfo.vue
+++ src/components/ProjectInfo.vue
@@ -1,64 +1,65 @@
 <template>
   <div class="columns is-marginless is-centered is-vcentered">
     <div class="column is-4 is-paddingless">
-	  <img :src=projectImage>
+      <img :src="projectImage" />
     </div>
-	<div class="column is-1"></div>
+    <div class="column is-1"></div>
     <div class="column is-7 is-paddingless">
       <p class="entry-subtitle">{{projectTitle}}</p>
       <h3>{{projectDescription}}</h3>
-	  <h3 class="project-link"><a :href=projectLink target="_blank">{{projectSite}}</a></h3>
+      <h3 class="project-link">
+        <a :href="projectLink" target="_blank">{{projectSite}}</a>
+      </h3>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-	props: {
-		projectImage: {
-			type: String,
-			required: true,
-		},
-		projectLink: {
-			type: String,
-			required: true,
-		},
-		projectSite: {
-			type: String,
-			required: true,
-		},
-		projectTitle: {
-			type: String,
-			required: true,
-		},
-		projectDescription: {
-			type: String,
-			required: true,
-		},
-	},
-	name: "ProjectInfo",
+  props: {
+    projectImage: {
+      type: String,
+      required: true
+    },
+    projectLink: {
+      type: String,
+      required: true
+    },
+    projectSite: {
+      type: String,
+      required: true
+    },
+    projectTitle: {
+      type: String,
+      required: true
+    },
+    projectDescription: {
+      type: String,
+      required: true
+    }
+  },
+  name: "ProjectInfo"
 };
 </script>
 
 <style scoped>
 .entry-title {
-	font-size: 1.5em;
-	color: var(--dark-text-colour);
-	font-family: "Raleway", sans-serif;
-	font-weight: bold;
-	margin-top: -0.2em;
+  font-size: 1.5em;
+  color: var(--dark-text-colour);
+  font-family: "Raleway", sans-serif;
+  font-weight: bold;
+  margin-top: -0.2em;
 }
 
 .entry-subtitle {
-	font-size: 1.2em;
-	color: var(--dark-text-colour);
-	font-family: "Raleway", sans-serif;
-	font-weight: bold;
-	margin-bottom: 0.2em;
+  font-size: 1.2em;
+  color: var(--dark-text-colour);
+  font-family: "Raleway", sans-serif;
+  font-weight: bold;
+  margin-bottom: 0.2em;
 }
 
 .project-link {
-	margin-top: 0.25em;
+  margin-top: 0.25em;
 }
 </style>
