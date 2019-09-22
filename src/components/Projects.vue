512acb16354afee264e862761a4125ec95e7ddd5
ClementTsang <clementjhtsang@gmail.com>
2019-09-22 02:48:55 -0400

    More changes, mainly fixed bug with mobile and FA icon area

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,30 +2,32 @@
   <div id="projects" class="columns is-marginless is-centered page-item">
     <div class="column is-10 is-8-desktop">
       <article>
-        <p class="title has-text-centered">Projects</p>
+        <p class="title has-text-centered">
+          Projects
+        </p>
         <hr class="is-divider" />
         <ProjectInfo
-          projectImage="./assets/bottom.gif"
-          projectLink="https://github.com/ClementTsang/bottom"
-          projectSite="GitHub"
-          projectTitle="bottom"
-          projectDescription="A top clone, written in Rust."
+          project-image="./assets/bottom.gif"
+          project-link="https://github.com/ClementTsang/bottom"
+          project-site="GitHub"
+          project-title="bottom"
+          project-description="A top clone, written in Rust."
           style="padding-bottom: 2rem;"
         />
         <ProjectInfo
-          projectImage
-          projectLink="https://github.com/ClementTsang/RustNAO"
-          projectSite="GitHub"
-          projectTitle="RustNAO"
-          projectDescription="A Rust wrapper for the SauceNAO API, a popular reverse image search engine."
+          project-image
+          project-link="https://github.com/ClementTsang/RustNAO"
+          project-site="GitHub"
+          project-title="RustNAO"
+          project-description="A Rust wrapper for the SauceNAO API, a popular reverse image search engine."
           style="padding-bottom: 2rem;"
         />
         <ProjectInfo
-          projectImage
-          projectLink="https://github.com/ClementTsang/rustcc"
-          projectSite="GitHub"
-          projectTitle="rustcc"
-          projectDescription="A simple C compiler, written from scratch in Rust."
+          project-image
+          project-link="https://github.com/ClementTsang/rustcc"
+          project-site="GitHub"
+          project-title="rustcc"
+          project-description="A simple C compiler, written from scratch in Rust."
           style="padding-bottom: 2rem;"
         />
       </article>
