52243366f589f6c022fd53e45a6c158d046e6c5e
ClementTsang <clementjhtsang@gmail.com>
2019-11-24 22:13:47 -0500

    Update gitignore.

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'plugin:vue/strongly-recommended',
+    'prettier'
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'vue/max-attributes-per-line': [
+      'warn',
+      {
+        singleline: 5,
+        multiline: {
+          max: 5,
+          allowFirstLine: false
+        }
+      }
+    ],
+    'vue/html-self-closing': [
+      'warn',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always'
+        },
+        svg: 'always',
+        math: 'always'
+      }
+    ]
+  }
+};
