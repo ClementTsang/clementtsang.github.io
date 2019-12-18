a3d2372def719bef50b563e2a6f98aedb9a77e2b
ClementTsang <clementjhtsang@gmail.com>
2019-12-18 18:03:54 -0500

    Add purgecss

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,22 @@
+const PurgecssPlugin = require("purgecss-webpack-plugin");
+const glob = require("glob-all");
+const path = require("path");
+
 module.exports = {
   configureWebpack: {
     optimization: {
       splitChunks: {
         minSize: 1
       }
-    }
+    },
+    plugins: [
+      new PurgecssPlugin({
+        paths: glob.sync([
+          path.join(__dirname, "./src/index.html"),
+          path.join(__dirname, "./**/*.vue"),
+          path.join(__dirname, "./src/**/*.js")
+        ])
+      })
+    ]
   }
 };
