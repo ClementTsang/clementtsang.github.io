6b04b7a565e19ac2b7972082a33b60f49fc75a74
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2023-02-16 01:39:57 -0500

    add lucide icons

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Personal Site
 
-Source code for my personal website, hosted using GitHub Pages [over here](https://clementtsang.github.io/), using Svelte.
+Source code for my personal website, hosted using GitHub Pages [over here](https://clementtsang.github.io/), using [Svelte](https://svelte.dev/), with icons from [Lucide](https://lucide.dev/).
 
 Feel free to use this design for your own use/inspiration.
 
--- src/lib/components/ExperienceCard.svelte
+++ src/lib/components/ExperienceCard.svelte
@@ -1,19 +1,33 @@
 <script lang="ts">
 	import type { Experience } from '../../types/Experience';
+	import Calendar from './icons/Calendar.svelte';
+	import MapPin from './icons/MapPin.svelte';
+	import Link from './icons/Link.svelte';
 
 	export let exp: Experience;
 </script>
 
 <div class="card">
 	<div class="work-info">
-		<div>
+		<div class="work-details">
 			<h4>{exp.name}</h4>
-			{#each exp.dates as date}
-				<p>{date}</p>
-			{/each}
-			<p>{exp.location}</p>
+			<div class="detail">
+				<Calendar size="1.1rem" />
+				<div class="dates">
+					{#each exp.dates as date}
+						<p>{date}</p>
+					{/each}
+				</div>
+			</div>
+			<div class="detail">
+				<MapPin size="1.1rem" />
+				<p>{exp.location}</p>
+			</div>
 			{#if exp.link}
-				<p><a href={exp.link.url} target="_blank" rel="noreferrer">{exp.link.name}</a></p>
+				<div class="detail">
+					<Link size="1.1rem" />
+					<p><a href={exp.link.url} target="_blank" rel="noreferrer">{exp.link.name}</a></p>
+				</div>
 			{/if}
 		</div>
 	</div>
@@ -41,6 +55,22 @@
 	.work-description {
 		grid-row: 1;
 		grid-column: 6 / -1;
+	}
+
+	.work-details {
+		display: flex;
+		flex-direction: column;
+		row-gap: 2px;
+	}
+
+	.detail {
+		display: flex;
+		align-items: center;
+		column-gap: 4px;
+	}
+
+	.dates {
+		display: inline-table;
 	}
 
 	/* TODO: Add responsiveness for cards. */
