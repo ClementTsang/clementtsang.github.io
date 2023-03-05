f96e3d4cc25d04f42e0c070468bdba8a4129b616
Clement Tsang <34804052+ClementTsang@users.noreply.github.com>
2023-03-05 04:23:20 -0500

    Optimize fonts (#46)
    * use locally hosted fonts rather than fontsource
    
    * preloading
    
    * fmt

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,5 @@
 <script>
-	// We set up fonts following this: https://fontsource.org/docs/variable-fonts
-
-	// Fallback weighted fonts.
-	import '@fontsource/raleway/400.css';
-	import '@fontsource/raleway/500.css';
-	import '@fontsource/quicksand/400.css';
-	import '@fontsource/quicksand/600.css';
-
-	// Variable fonts.
-	import '@fontsource/raleway/variable.css';
-	import '@fontsource/quicksand/variable.css';
-
+	import '../styles/fonts.scss';
 	import '../styles/app.scss';
 	import '../styles/text.scss';
 </script>
