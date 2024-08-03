92125ddd5785aaf36883013af00822b33c09f5c7
ClementTsang <34804052+ClementTsang@users.noreply.github.com>
2024-08-03 00:42:58 -0400

    update some icons

--- src/lib/components/icons/Mail.svelte
+++ src/lib/components/icons/Mail.svelte
@@ -2,7 +2,7 @@
 	export let size: number | string = 24;
 </script>
 
-<!--Icons are from https://lucide.dev/-->
+<!--Icons are from https://lucide.dev/ -->
 <svg
 	xmlns="http://www.w3.org/2000/svg"
 	width={size}
