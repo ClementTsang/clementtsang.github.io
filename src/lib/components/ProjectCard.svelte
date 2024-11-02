656823ee4f291c4234833a4d776b095ecc45d36a
Clement Tsang <34804052+ClementTsang@users.noreply.github.com>
2024-11-02 12:57:06 -0400

    migrate to svelte 4 (#69)
    * migrate to svelte 4
    
    * update eslint with typescript config
    
    * also fix CI
    
    * fix eslint

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -43,7 +43,9 @@
 		background-size: cover;
 		overflow: hidden;
 
-		box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
+		box-shadow:
+			0 2px 3px rgba(10, 10, 10, 0.1),
+			0 0 0 1px rgba(10, 10, 10, 0.1);
 
 		border-width: 0px;
 		border-radius: 4px;
